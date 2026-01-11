315ff17f3b754d72d3b0b26e93d7738720667f5b
safaaa2024 <safaa2000.ahmed@gmail.com>
2026-01-11 18:31:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// function copyText() {
+//     var text = document.getElementById("germanText").innerText;
+    
+//     navigator.clipboard.writeText(text).then(function() {
+//         alert("Text copied: " + text);
+//     });
+// }
+// // copyText();
